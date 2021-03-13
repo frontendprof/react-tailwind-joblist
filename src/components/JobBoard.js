027e778508d7f164b668bef36dcc4a73490ef6ca
frontendprof <https://github.com/frontendprof>
2021-03-13 11:00:24 +0500

    some style

--- src/components/JobBoard.js
+++ src/components/JobBoard.js
@@ -17,34 +17,35 @@ const JobBoard = ({job:{company,tools,logo,featured,position,level,role,location
     }
 
     return (
-        <div className={`flex bg-blue-100 shadow-lg m-4 p-6 rounded w-11/12 ml-16
+        <div className={`flex flex-col bg-blue-100 shadow-lg my-16 mx-10 p-6 rounded w-11/12 ml-16 sm:flex-row
             ${featured&& 'border-l-4 border-indigo-800 border-solid'}
         `}>
             
             <div>
-                <img src={logo} alt={company}/>
+                <img className=" -mt-16 mb-4 w-20 h-20 sm:my-0 sm:h-24 sm:w-24" src={logo} alt={company}/>
             </div>
 
             <div className="ml-4 flex flex-col justify-between">
                 <h3 className="font-bold text-purple-500">
                     {company}
                     {isNew&&<span className="text-gray-600 bg-yellow-500
-                    font-normal m-2 px-1 py-2 rounded-full">New</span>}
-                    {featured&&<span className="text-yellow-300 bg-pink-700
-                    font-normal m-2 p-2 rounded-full">Featured</span>}
+                    font-normal m-2 px-1 py-2 rounded-full uppercase text-sm">New</span>}
+                    {featured&&<span className="text-yellow-300 bg-pink-700 uppercase
+                    font-normal m-2 p-2 rounded-full text-sm">Featured</span>}
 
                 </h3>
-                <h2 className="font-bold text-xl">{position}</h2>
+                <h2 className="font-bold text-xl my-2">{position}</h2>
                 <p className="text-gray-700">
                     {postedAt}&middot;{contract}&middot;{location}
                 </p>
 
             </div>
 
-            <div className="flex ml-auto items-center">
+            <div className="flex mt-4 mx-4 pt-4 border-gray-300 border-solid border-t items-center flex-wrap 
+                sm:ml-auto sm:border-0 sm:pt-0 sm:mt-0">
                 {tags?(
                     tags.map((tag,ind)=><span key={ind} className="text-indigo-400 bg-indigo-100
-                    font-bold m-2 p-1 rounded-xl">{tag}</span>)
+                    font-bold mr-4 mb-2 p-1 rounded-xl sm:mb-0">{tag}</span>)
                 ):""}
             </div>
         </div>
